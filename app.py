b45728351dcec27ee8c8d7a1408d2cb85558c226
Ranjeet Kumar Mahto <iamranjeet2572@gmail.com>
2026-04-12 19:02:35 +0530

    fix model deployment

--- app.py
+++ app.py
@@ -1552,12 +1552,14 @@ def leaderboard():
 
 
 
-
-
-
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+
+# if __name__ == "__main__":
+#     app.run()
     
     
     
